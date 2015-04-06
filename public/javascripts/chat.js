26db49fb4a813ea59b12adc72c55eba404e45b06
Ira Levin <ira.levin@datalink.com>
2015-04-05 20:27:30 -0700

    Initial commit

---
+++ public/javascripts/chat.js
@@ -0,0 +1,18 @@
+// var socket = io.connect('/'); 
+
+// socket.on('message', function (data) { 
+//   data = JSON.parse(data); 
+//   $('#messages').append('<div class="' + data.type + '">' + data.message + '</div>'); 
+// }); 
+
+// Use jQuery document ready function to place send button click handler
+$(function(){ 
+  $('#send').click(function(){ 
+    var data = { 
+      message: $('#message').val(), 
+      type:'userMessage' 
+    }; 
+    socket.send(JSON.stringify(data)); 
+    $('#message').val(''); 
+  }); 
+}); 
